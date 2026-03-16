b5cb38fbcad05e23e64240a4be85aab82c2355b3
Brookyan2023 <133002531+Brookyan2023@users.noreply.github.com>
2026-03-16 17:28:44 +0800

    Update MCP button icons

--- plugin/Core/Application.cs
+++ plugin/Core/Application.cs
@@ -16,15 +16,15 @@ namespace revit_mcp_plugin.Core
             PushButtonData pushButtonData = new PushButtonData("ID_EXCMD_TOGGLE_REVIT_MCP", "Revit MCP\r\n Switch",
                 Assembly.GetExecutingAssembly().Location, "revit_mcp_plugin.Core.MCPServiceConnection");
             pushButtonData.ToolTip = "Open / Close mcp server";
-            pushButtonData.Image = new BitmapImage(new Uri("/RevitMCPPlugin;component/Core/Ressources/icon-16.png", UriKind.RelativeOrAbsolute));
-            pushButtonData.LargeImage = new BitmapImage(new Uri("/RevitMCPPlugin;component/Core/Ressources/icon-32.png", UriKind.RelativeOrAbsolute));
+            pushButtonData.Image = new BitmapImage(new Uri("/RevitMCPPlugin;component/Core/Ressources/mcp-starter.png", UriKind.RelativeOrAbsolute));
+            pushButtonData.LargeImage = new BitmapImage(new Uri("/RevitMCPPlugin;component/Core/Ressources/mcp-starter.png", UriKind.RelativeOrAbsolute));
             PushButton toggleButton = mcpPanel.AddItem(pushButtonData) as PushButton;
 
             PushButtonData mcp_settings_pushButtonData = new PushButtonData("ID_EXCMD_MCP_SETTINGS", "Settings",
                 Assembly.GetExecutingAssembly().Location, "revit_mcp_plugin.Core.Settings");
             mcp_settings_pushButtonData.ToolTip = "MCP Settings";
-            mcp_settings_pushButtonData.Image = new BitmapImage(new Uri("/RevitMCPPlugin;component/Core/Ressources/settings-16.png", UriKind.RelativeOrAbsolute));
-            mcp_settings_pushButtonData.LargeImage = new BitmapImage(new Uri("/RevitMCPPlugin;component/Core/Ressources/settings-32.png", UriKind.RelativeOrAbsolute));
+            mcp_settings_pushButtonData.Image = new BitmapImage(new Uri("/RevitMCPPlugin;component/Core/Ressources/mcp-settings.png", UriKind.RelativeOrAbsolute));
+            mcp_settings_pushButtonData.LargeImage = new BitmapImage(new Uri("/RevitMCPPlugin;component/Core/Ressources/mcp-settings.png", UriKind.RelativeOrAbsolute));
             mcpPanel.AddItem(mcp_settings_pushButtonData);
 
             // Auto Millwork Sheet is now provided by the BIMEfficientTool add-in.
